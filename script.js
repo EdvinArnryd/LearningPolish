eaa4d2453d7ded68475c9819587041b88f5d8f1a
Edvin Arnryd <edvin.arn@hotmail.com>
2026-09-19 23:46:22 +0200

    added color and styling

--- script.js
+++ script.js
@@ -16,15 +16,32 @@ const monthsArr = [
     {english: "December", polish: "Grudzień"},
 ]
 
+const monthColor = [
+    {color: "cornflowerblue"},
+    {color: "pink"},
+    {color: "green"},
+    {color: "lawngreen"},
+    {color: "greenyellow"},
+    {color: "yellow"},
+    {color: "orange"},
+    {color: "red"},
+    {color: "brown"},
+    {color: "orangered"},
+    {color: "purple"},
+    {color: "blue"},
+]
+
 
 for(let i = 0; i < months; i++)
 {
     let myDiv = document.createElement("div");
     myDiv.className = "myDiv";
     myDiv.textContent = monthsArr[i].english;
+    myDiv.style.backgroundColor = monthColor[i].color;
 
     let plDiv = document.createElement("div");
     plDiv.textContent = monthsArr[i].polish;
+    plDiv.className = "month";
     container.appendChild(myDiv);
     myDiv.appendChild(plDiv);
 }
